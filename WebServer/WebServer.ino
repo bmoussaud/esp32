18e5e4271afe3eb3e72b4232a09299b31a810ade
Benoit Moussaud <bmoussaud@xebialabs.com>
2021-01-24 19:39:25 +0100

    mv to folder

---
+++ WebServer/WebServer.ino
@@ -0,0 +1,21 @@
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 2
+#endif
+
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+    Serial.begin(9600);
+    // initialize digital pin LED_BUILTIN as an output.
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop()
+{
+    Serial.println("Hello....2");
+    digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+    delay(1000);                     // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+    delay(1000);                     // wait for a second
+}
